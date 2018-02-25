360fe68f188f8bbe1f0a2d6e88ab53e760e4ba00
Celina <celina.kruczkowska@gmail.com>
2018-02-25 12:08:05 +0100

    lazyLoad

--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -3,6 +3,7 @@ import { string, number } from 'prop-types';
 import { Link } from 'react-router-dom';
 import ListRating from '../ListRating';
 import Picture from '../Picture';
+import LazyLoad from 'react-lazyload';
 import BottleSvg from '../../svg-components/Bottle';
 import './style.scss';
 
@@ -10,45 +11,47 @@ class ListItem extends PureComponent {
   render() {
     return (
       <li>
-        <div className="ListItem">
-          <article>
-            <section className="ListItem__main">
-              <div className="ListItem__photo-container">
-                {this.props.image_url ? (
-                  <Picture className="ListItem__photo" src={this.props.image_url} alt={this.props.name} ></Picture>      
-                ) : (
-                  <BottleSvg className="ListItem__photo" />
-                )}
-              </div>
-
-              <div className="ListItem__description">
-                <h2 className="ListItem__annotation ListItem__annotation--small">{this.props.kind}</h2>
-
-                <Link to={`/wine/${this.props.id}`}>
-                  <h1 className="ListItem__title">{this.props.name}</h1>
-                </Link>
-                <p className="ListItem__annotation">
-                  {this.props.country_code && (
-                    <img
-                      className="ListItem__icon"
-                      src={`images/country-flags/${this.props.country_code}.svg`}
-                      alt=""
-                    />
+        <LazyLoad height={150}>
+          <div className="ListItem">
+            <article>
+              <section className="ListItem__main">
+                <div className="ListItem__photo-container">
+                  {this.props.image_url ? (
+                    <Picture className="ListItem__photo" src={this.props.image_url} alt={this.props.name} ></Picture>
+                  ) : (
+                    <BottleSvg className="ListItem__photo" />
                   )}
-                  <span>{this.props.region}</span>
-                </p>
-              </div>
-            </section>
+                </div>
 
-            <footer className="ListItem__footer">
-              <ListRating className="ListItem__footer-item" rating={this.props.rating} votesCount={this.props.nr_of_ratings} />
+                <div className="ListItem__description">
+                  <h2 className="ListItem__annotation ListItem__annotation--small">{this.props.kind}</h2>
 
-              <Link to={`/wine/${this.props.id}`} className="ListItem__footer-item ListItem__footer-action">
-                buy (${this.props.price})
-              </Link>
-            </footer>
-          </article>
-        </div>
+                  <Link to={`/wine/${this.props.id}`}>
+                    <h1 className="ListItem__title">{this.props.name}</h1>
+                  </Link>
+                  <p className="ListItem__annotation">
+                    {this.props.country_code && (
+                      <img
+                        className="ListItem__icon"
+                        src={`images/country-flags/${this.props.country_code}.svg`}
+                        alt=""
+                      />
+                    )}
+                    <span>{this.props.region}</span>
+                  </p>
+                </div>
+              </section>
+
+              <footer className="ListItem__footer">
+                <ListRating className="ListItem__footer-item" rating={this.props.rating} votesCount={this.props.nr_of_ratings} />
+
+                <Link to={`/wine/${this.props.id}`} className="ListItem__footer-item ListItem__footer-action">
+                  buy (${this.props.price})
+                </Link>
+              </footer>
+            </article>
+          </div>
+        </LazyLoad>
       </li>
     );
   }
